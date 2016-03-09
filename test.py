c91379618914340feeeb49e65f90fcf5b13d58b3
Henrique Dias <heukirne@gmail.com>
2016-03-09 09:22:25 -0300

    update

--- test.py
+++ test.py
@@ -1,16 +1,27 @@
 #!/usr/bin/python
 # -*- coding: utf-8 -*-
 import os, sys
+import chardet
+
 sys.path.append("./pylinguistics/pylinguistics/")
 
 import Pylinguistics as pl
 import pandas as pd
 
-text = "O rato roeu a roupa do rei de Roma."
+if 'TRAVIS' in os.environ and os.environ['TRAVIS'] == 'yes':
+    #SAMPLE TEXT
+    text = "O rato roeu a roupa do rei de Roma."
+    print text
+    objpl = pl.text(text)
+else:
+    # READ FILE
+    file = open('reviews/Celular e Smartphone/5.0/0_100112.txt', 'r')
+    text = file.read()
+    
+    print('Charset: %s' %chardet.detect(text))
+    
+    objpl = pl.text(text.decode(chardet.detect(text)['encoding']))
 
-print text
-
-objpl = pl.text(text)
 
 objpl.setLanguage("pt-br");
 
@@ -18,6 +29,3 @@ print('Features: %s' %objpl.getFeatures())
 #print('POS_TAGS: %s' %objpl.tokens)
 print('POS_TAGS: %s' %objpl.postag)
 #print(len(objpl.postag))
-
-
- 
