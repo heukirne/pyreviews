36af409102b15a95bf57cfb6523f7c65a7be7e5e
Henrique Dias <heukirne@gmail.com>
2016-03-10 13:24:16 -0300

    improve clean script

--- cleanreviews.sh
+++ cleanreviews.sh
@@ -1,7 +1,11 @@
 cd reviews
 find -name "* *" -type d | rename 's/ /_/g'
 find . -name "*.txt" -type f -delete
-for file in $(pcregrep -MlrI "<thumbsUp value=\"0\"/>(\n).*<thumbsDown value=\"0\"/>")
-do 
-	rm $file
+for dir in $(ls)
+do
+	echo "removing from $dir"
+	for file in $(pcregrep -MlrI "<thumbsUp value=\"0\"/>(\n).*<thumbsDown value=\"0\"/>" $dir)
+	do 
+		rm $file
+	done
 done
